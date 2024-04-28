f3a23132a5cdfa79476f84e0fd238d3f4522e45b
Noah Cavestany <ncavestany@dons.usfca.edu>
2024-04-27 23:58:07 -0700

    fixed overlapping zoom

--- js/choropleth.js
+++ js/choropleth.js
@@ -240,7 +240,7 @@ Promise.all([
     var zoom = d3.zoom()
         .scaleExtent([1, 8])
         .on('zoom', function (event) {
-            d3.selectAll('path')
+            svg.selectAll('path')
                 .attr('transform', event.transform);
         });
 
